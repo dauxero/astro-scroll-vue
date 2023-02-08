98371b5c2637ccb01e94de809812455b547db4f3
dauxero <69281850+dauxero@users.noreply.github.com>
2023-02-08 12:18:40 -0500

    Add files via upload

---
+++ src/components/ScrollPage.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="slider" ref="slider">
+    <div class="navvar">
+      <a href="#" @click="moveScroll(1)">Home</a>
+      <a href="#" @click="moveScroll(2)">Dos</a>
+      <a href="#" @click="moveScroll(3)">Tres</a>
+      <a href="#" @click="moveScroll(4)">Cuatro</a>
+    </div>
+    <div class="content_page">
+      <h1>Home</h1>
+    </div>
+    <div class="content_page">
+      <h1>Cotenido 2</h1>
+    </div>
+    <div class="content_page">
+      <h1>Contenido 3</h1>
+    </div>
+    <div class="content_page">
+      <h1>Contenido 4</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      slider: null,
+      height: null,
+    };
+  },
+  mounted() {
+    this.slider = this.$refs.slider;
+    this.height = this.slider.offsetHeight;
+  },
+  methods: {
+    moveScroll(n) {
+      n = n - 1;
+      this.slider.scrollTo({
+        top: this.height * n,
+        behavior: "smooth",
+      });
+    },
+  },
+};
+</script>
+
+<style>
+html,
+body {
+  height: 100%;
+  margin: 0;
+  padding: 0;
+  font-family: Arial, Helvetica, sans-serif;
+}
+.slider {
+  position: relative;
+  height: 100vh;
+  overflow: scroll;
+}
+
+.content_page {
+  height: 100vh;
+  background-color: whitesmoke;
+  color: black;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.navvar {
+  overflow: hidden;
+  background-color: black;
+  position: fixed;
+  top: 0;
+  width: 100%;
+}
+
+.navvar a {
+  float: left;
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+}
+
+.navvar a:hover {
+  background-color: gray;
+  color: black;
+}
+</style>
